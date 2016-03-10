d78737f8c72d236d2e547d00c1dd3052eb5b4761
bilalrashid <i120527@nu.edu.pk>
2016-03-10 20:58:54 +0500

    other commit

---
+++ myOtherPackage.go
@@ -0,0 +1,7 @@
+//Package greetings merge 2 strings
+package greetings
+
+//Merge is the funtion which merges 2 strings and return a single string
+func Merge(str1 string, str2 string) string {
+	return str1 + "" + str2
+}
